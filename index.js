d1307020db09360acc47454faa63c43830ba1b3e
Matthew <+>
2023-10-11 16:32:28 +0300

    Completed looping code

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards() {
+  const cards = ["Guadalupe", "Ollie", "Aki"];
+  const surprise = "surprise";
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]}, for the wonderful ${surprise} gift!`);
+  }
+  return cards;
+}
+writeCards();
+
+function countDown(number) {
+  let i = number;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
+countDown(10);
